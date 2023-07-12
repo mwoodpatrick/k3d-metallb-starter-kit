f2357ae61ed9b22c74c9d1563c8c053973cbb9c7
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-12 10:41:26 -0700

    add wait for metrics server, cleanup scripts

--- scripts/simple/create-cluster.sh
+++ scripts/simple/create-cluster.sh
@@ -2,9 +2,9 @@
 set -x
 k3d --version
 k3d cluster create containers-fan-cluster --wait
-kubectl -n kube-system wait deployment.apps/metrics-server --for=condition=Ready
+kubectl -n kube-system wait deployment.apps/metrics-server --for=condition=Available
+time kubectl -n kube-system wait apiservices v1beta1.metrics.k8s.io  --for=condition=Available --timeout=5m
+kubectl get apiservices
 kubectl cluster-info
 kubectl config get-contexts
 kubectl get nodes --output wide
-k3d cluster delete containers-fan-cluster
-kubectl config get-contexts
---
+++ scripts/simple/teardown-cluster.sh
@@ -0,0 +1,3 @@
+set -x
+k3d cluster delete containers-fan-cluster
+kubectl config get-contexts
