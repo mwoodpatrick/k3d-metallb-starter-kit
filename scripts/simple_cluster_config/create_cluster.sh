6aa7573f3718fb172d5bff38bf028b632135245a
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-12 20:06:22 -0700

    update config working now with latest k3s image and 3 worker nodes

--- scripts/simple_cluster_config/create_cluster.sh
+++ scripts/simple_cluster_config/create_cluster.sh
@@ -1,3 +1,4 @@
+# create the k3d cluster
 # https://containers.fan/posts/using-k3d-to-run-development-kubernetes-clusters/
 # https://github.com/k3d-io/k3d/pkgs/container/k3d-proxy
 
@@ -5,9 +6,42 @@ set -x
 
 k3d --version
 k3d cluster create --config cluster-config.yml --wait
+# k3d cluster create westie-dev --servers 1 --agents 3 --k3s-arg "--disable=traefik@server:0" --wait --volume /run/udev:/run/udev --volume /mnt/wsl/projects:/projects
 kubectl -n kube-system wait deployment.apps/metrics-server --for=condition=Available
 time kubectl -n kube-system wait apiservices v1beta1.metrics.k8s.io  --for=condition=Available --timeout=5m
 kubectl get apiservices
 kubectl cluster-info
 kubectl config get-contexts
 kubectl get nodes --output wide
+
+# determine loadbalancer ingress range
+cidr_block=$(docker network inspect k3d-westie-dev-config | jq '.[0].IPAM.Config[0].Subnet' | tr -d '"')
+cidr_base_addr=${cidr_block%???}
+ingress_first_addr=$(echo $cidr_base_addr | awk -F'.' '{print $1,$2,255,0}' OFS='.')
+ingress_last_addr=$(echo $cidr_base_addr | awk -F'.' '{print $1,$2,255,255}' OFS='.')
+ingress_range=$ingress_first_addr-$ingress_last_addr
+
+# deploy metallb 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
+
+# configure metallb ingress address range
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - $ingress_range
+EOF
+
+# set kubeconfig to access the k8s context
+export KUBECONFIG=$(k3d kubeconfig write westie-dev-config)
+
+# validate the cluster master and worker nodes
+kubectl get nodes
