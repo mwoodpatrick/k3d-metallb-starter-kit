48f89588cafb7bf939f2aeade03b192be03ef12e
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-14 06:34:29 -0700

    fix whoami deployment

--- scripts/simple_cluster_config/apps/whoami/install.sh
+++ scripts/simple_cluster_config/apps/whoami/install.sh
@@ -1,7 +1,6 @@
 # https://faun.pub/kubernetes-ingress-with-nginx-3c77e703e91a
 
 kubectl create namespace whoami 
-kubectl apply -n whoami -f deployment.yaml
-kubectl apply -n whoami -f service.yaml
-kubectl -n whoami get all
-kubectl -n whoami port-forward service/whoami-service :80 &
+kubectl apply -f whoami.yaml
+kubectl get all
+# kubectl -n whoami port-forward service/whoami-service :80 &
