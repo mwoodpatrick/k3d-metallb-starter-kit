9e29501643efc752d992386370917de31b492dcd
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-16 12:18:37 -0700

    minor fixes to work woth nginx ingress

--- scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
+++ scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
@@ -1,8 +1,8 @@
 set -x
-NS=wordpress
+NS=default
 helm show values bitnami/wordpress 
-helm install wordpress bitnami/wordpress -n $NS --create-namespace --set serviceType=NodePor --set  global.storageClass=local-path --values values.yaml 
+helm install wordpress bitnami/wordpress -n $NS --create-namespace --set serviceType=NodePort --set  global.storageClass=local-path --values values.yaml 
 kubectl api-resources
 kubectl get ingressclasses
-k describe ingressclass traefik
+kubectl describe ingressclass nginx
 kubectl -n $NS
