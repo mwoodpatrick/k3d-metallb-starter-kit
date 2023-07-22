f307afac53d9e3b428a08268d2c59aed92615ae1
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-22 06:03:46 -0700

    update dashboard install

--- scripts/simple_cluster_config/apps/dashboard/install.sh
+++ scripts/simple_cluster_config/apps/dashboard/install.sh
@@ -1,6 +1,20 @@
-# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
-# https://www.linuxtechi.com/how-to-install-kubernetes-dashboard/
 set -x
+# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
+export VERSION_KUBE_DASHBOARD=v2.7.0
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+
+kubectl proxy &
+kubectl -n kubernetes-dashboard create token admin-user
+
+# open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/pod?namespace=kubernetes-dashboard and paste token
+
+# https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+kubectl apply -f dashboard-adminuser.yaml
+kubectl apply -f dashboard.admin-user-role.yaml
+kubectl -n kubernetes-dashboard create token admin-user
+
+# https://www.linuxtechi.com/how-to-install-kubernetes-dashboard/
 
 # helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 # helm repo list
@@ -10,16 +24,14 @@ set -x
 # kubectl create -f k8s-dashboard-account.yaml
 # kubectl -n kube-system create token admin-user
 
+
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
 
-export VERSION_KUBE_DASHBOARD=v2.7.0
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
 
 kubectl create -f dashboard.admin-user.yaml -f dashboard.admin-user-role.yaml
 # kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
 kubectl -n kubernetes-dashboard create token admin-user
-kubectl proxy
 
 echo http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
