6572cc3b714b64524eb2093865b39ee5a97e800f
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-22 07:02:12 -0700

    get wordpress working with nginx ingress

--- scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
+++ scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
@@ -1,8 +1,10 @@
 set -x
-NS=default
+NS=wordpress
 helm show values bitnami/wordpress 
 helm install wordpress bitnami/wordpress -n $NS --create-namespace --set serviceType=NodePort --set  global.storageClass=local-path --values values.yaml 
+k apply -f wordpress_ingress.yaml
 kubectl api-resources
 kubectl get ingressclasses
 kubectl describe ingressclass nginx
-kubectl -n $NS
+kubectl -n $NS get all
+kubectl -n $NS exec -it wordpress-76b55f565f-rv78k -- bash
