118904aac3d42d83feb9ea53b30e368d58e418b2
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-22 11:53:35 -0700

    add some testing commands, working on using openebs

--- scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
+++ scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
@@ -7,4 +7,21 @@ kubectl api-resources
 kubectl get ingressclasses
 kubectl describe ingressclass nginx
 kubectl -n $NS get all
-kubectl -n $NS exec -it wordpress-76b55f565f-rv78k -- bash
+
+WORDPRESS_POD_NAME=`kubectl -n $NS get pods --selector=app.kubernetes.io/name=wordpress -o jsonpath='{.items[*].metadata.name}'`
+echo WORDPRESS_POD_NAME=$WORDPRESS_POD_NAME
+
+kubectl -n $NS logs $WORDPRESS_POD_NAME
+
+$GIT_ROOT/westie-dev-pnpm/kube/bitnami/containers/bitnami/wordpress/6/debian-11/rootfs/opt/bitnami/scripts/libwordpress.sh
+echo kubectl -n $NS exec -it wordpress-76b55f565f-rv78k -- bash
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin list
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin install bbpress --activate
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin install simple-history --activate
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp simple-history list
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin search gutenify
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin list
+
+# themes
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp theme list
+kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp theme search photo --per-page=6
