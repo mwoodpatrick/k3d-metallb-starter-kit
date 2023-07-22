a087bd366b1535f265ab0a526ae80d5c6832d9f5
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-22 12:29:04 -0700

    enable ebs

--- scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
+++ scripts/simple_cluster_config/apps/wordpress/deploy-wordpress.sh
@@ -2,18 +2,19 @@ set -x
 NS=wordpress
 helm show values bitnami/wordpress 
 helm install wordpress bitnami/wordpress -n $NS --create-namespace --set serviceType=NodePort --set  global.storageClass=local-path --values values.yaml 
-k apply -f wordpress_ingress.yaml
+helm install wordpress bitnami/wordpress -n $NS --create-namespace --set serviceType=NodePort --set  global.storageClass=openebs-hostpath --values values.yaml 
+kubectl apply -f wordpress_ingress.yaml
+watch kubectl -n $NS get all
 kubectl api-resources
 kubectl get ingressclasses
 kubectl describe ingressclass nginx
-kubectl -n $NS get all
 
 WORDPRESS_POD_NAME=`kubectl -n $NS get pods --selector=app.kubernetes.io/name=wordpress -o jsonpath='{.items[*].metadata.name}'`
 echo WORDPRESS_POD_NAME=$WORDPRESS_POD_NAME
 
 kubectl -n $NS logs $WORDPRESS_POD_NAME
 
-$GIT_ROOT/westie-dev-pnpm/kube/bitnami/containers/bitnami/wordpress/6/debian-11/rootfs/opt/bitnami/scripts/libwordpress.sh
+# $GIT_ROOT/westie-dev-pnpm/kube/bitnami/containers/bitnami/wordpress/6/debian-11/rootfs/opt/bitnami/scripts/libwordpress.sh
 echo kubectl -n $NS exec -it wordpress-76b55f565f-rv78k -- bash
 kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin list
 kubectl -n $NS exec $WORDPRESS_POD_NAME -- wp plugin install bbpress --activate
