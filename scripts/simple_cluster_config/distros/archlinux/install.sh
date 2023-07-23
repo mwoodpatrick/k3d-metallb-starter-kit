7554e671bea76bd70f1d2c798c50a3cdd936737c
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-23 08:04:59 -0700

    add /projects volume and report more details on install

--- scripts/simple_cluster_config/distros/archlinux/install.sh
+++ scripts/simple_cluster_config/distros/archlinux/install.sh
@@ -1,8 +1,48 @@
 # install archlinux distro
 
+function kubectl_getall_resources {
+  for i in $(kubectl api-resources --verbs=list --namespaced -o name | grep -v "events.events.k8s.io" | grep -v "events" | sort | uniq); do
+    echo "Resource:" $i
+    
+    if [ -z "$1" ]
+    then
+        kubectl get --ignore-not-found ${i}
+    else
+        kubectl -n ${1} get --ignore-not-found ${i}
+    fi
+  done
+}
+
+function kubectl_getall {
+    # get all names
+    NAMES=( $(kubectl api-resources \
+                      --namespaced \
+                      --verbs list \
+                      -o name) )
+    
+    # Now join names into single string delimited with comma
+    # Note *, not @
+    IFS=,
+    NAMES="${NAMES[*]}"
+    unset IFS
+    
+    # --show-kind is enabled implicitly
+    kubectl -n $1 get "$NAMES" -o wide --show-kind
+}
+
+export ARCHLINUX_NS=archlinux
 kubectl apply -f arch_linux.yaml
-kubectl -n archlinux get all
-kubectl -n archlinux get pvc
+export ARCHLINUX_POD_NAME=`kubectl -n $ARCHLINUX_NS get pods -l app=arch  -o jsonpath='{.items[*].metadata.name}'`
+# kubectl -n $ARCHLINUX_NS get all
+# kubectl -n $ARCHLINUX_NS get pvc
+kubectl_getall $ARCHLINUX_NS
+
 ls /mnt/wsl/projects/data/openebs/local-hostpath
-echo monitor creation using: watch -d kubectl -n archlinux get all
+
+alias exec_arch="kubectl -n $ARCHLINUX_NS exec -it $ARCHLINUX_POD_NAME -- bash"
+echo ARCHLINUX_NS=$ARCHLINUX_NS
+echo ARCHLINUX_POD_NAME=$ARCHLINUX_POD_NAME
+echo monitor creation using: watch -d kubectl -n $ARCHLINUX_NS get all
+echo use exec_arch to connect to distro
+
 
