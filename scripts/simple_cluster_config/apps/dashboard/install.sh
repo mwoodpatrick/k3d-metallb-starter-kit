e9ce40d73e73e40497aaf84d9caa35daa7f2f7bc
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-26 22:29:41 -0700

    fix issues with dasboard deployment

--- scripts/simple_cluster_config/apps/dashboard/install.sh
+++ scripts/simple_cluster_config/apps/dashboard/install.sh
@@ -4,15 +4,14 @@ export VERSION_KUBE_DASHBOARD=v2.7.0
 kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
-kubectl proxy &
-kubectl -n kubernetes-dashboard create token admin-user
+# kubectl proxy &
 
 # open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/pod?namespace=kubernetes-dashboard and paste token
 
 # https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
-kubectl apply -f dashboard-adminuser.yaml
+kubectl apply -f dashboard.admin-user.yaml
 kubectl apply -f dashboard.admin-user-role.yaml
-kubectl -n kubernetes-dashboard create token admin-user
+kubectl -n kubernetes-dashboard create token admin-user > admin-user.token
 
 # https://www.linuxtechi.com/how-to-install-kubernetes-dashboard/
 
@@ -29,9 +28,9 @@ kubectl -n kubernetes-dashboard create token admin-user
 
 
 
-kubectl create -f dashboard.admin-user.yaml -f dashboard.admin-user-role.yaml
+# kubectl create -f dashboard.admin-user.yaml -f dashboard.admin-user-role.yaml
 # kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
-kubectl -n kubernetes-dashboard create token admin-user
+# kubectl -n kubernetes-dashboard create token admin-user
 
 echo http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
 
--- scripts/simple_cluster_config/apps/dashboard/uninstall.sh
+++ scripts/simple_cluster_config/apps/dashboard/uninstall.sh
@@ -1,5 +1,7 @@
-kubectl delete ns kubernetes-dashboard
-kubectl delete clusterrolebinding kubernetes-dashboard
-kubectl delete clusterrole kubernetes-dashboard
+kubectl delete --ignore-not-found=true -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+kubectl delete --ignore-not-found=true ns/kubernetes-dashboard
+# kubectl delete ns kubernetes-dashboard
+# kubectl delete clusterrolebinding kubernetes-dashboard
+# kubectl delete clusterrole kubernetes-dashboard
 
 
