990f82adc38ea76274c28c8ba3be8e39c3c10e9a
mwoodpatrick <mwoodpatrick@gmail.com>
2023-08-03 07:18:26 -0700

    add more tests for install sites & fixup wordpress install

--- scripts/simple_cluster_config/apps/wordpress/install.sh
+++ scripts/simple_cluster_config/apps/wordpress/install.sh
@@ -6,10 +6,6 @@ helm show values bitnami/wordpress
 # helm install wordpress bitnami/wordpress -n $WORDPRESS_NS --create-namespace --set serviceType=NodePort --set  global.storageClass=local-path --values values.yaml 
 helm install wordpress bitnami/wordpress -n $WORDPRESS_NS --create-namespace --set serviceType=NodePort --set  global.storageClass=openebs-hostpath --values $wpdir/values.yaml 
 kubectl apply -f $wpdir/wordpress_ingress.yaml
-watch kubectl -n $WORDPRESS_NS get all
-kubectl api-resources
-kubectl get ingressclasses
-kubectl describe ingressclass nginx
 
 WORDPRESS_POD_NAME=`kubectl -n $WORDPRESS_NS get pods --selector=app.kubernetes.io/name=wordpress -o jsonpath='{.items[*].metadata.name}'`
 echo WORDPRESS_POD_NAME=$WORDPRESS_POD_NAME
@@ -17,8 +13,9 @@ echo WORDPRESS_POD_NAME=$WORDPRESS_POD_NAME
 kubectl -n $WORDPRESS_NS logs $WORDPRESS_POD_NAME
 
 # $GIT_ROOT/westie-dev-pnpm/kube/bitnami/containers/bitnami/wordpress/6/debian-11/rootfs/opt/bitnami/scripts/libwordpress.sh
-echo kubectl -n $WORDPRESS_NS exec -it wordpress-76b55f565f-rv78k -- bash
+echo kubectl -n $WORDPRESS_NS exec -it $WORDPRESS_POD_NAME -- bash
 kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp plugin list
+kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp plugin install wordpress-beta-tester --activate
 kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp plugin install bbpress --activate
 kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp plugin install simple-history --activate
 kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp simple-history list
@@ -28,3 +25,5 @@ kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp plugin list
 # themes
 kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp theme list
 kubectl -n $WORDPRESS_NS exec $WORDPRESS_POD_NAME -- wp theme search photo --per-page=6
+
+watch kubectl -n $WORDPRESS_NS get all
--- scripts/simple_cluster_config/install.sh
+++ scripts/simple_cluster_config/install.sh
@@ -10,7 +10,15 @@ mkdir -p logs
 . load_balancer/metallb/install.sh 2>&1 | tee logs/install_load_balancer_metallb.log
 . apps/dashboard/install.sh 2>&1 | tee logs/install_apps_dashboard.log
 . ingress/nginx/install.sh 2>&1 | tee logs/install_ingress_nginx.log
+# kubectl get ingressclasses
+# kubectl describe ingressclass nginx
 . service/load_balancer/minikube/install.sh 2>&1 | tee logs/install_load_balancer_minikube.log
 . storage/openebs/install.sh 2>&1 | tee logs/install_openebs.log
+# k get sc
 . apps/wordpress/install.sh 2>&1 | tee logs/install_wordpress.log
+# access http://wp.westie.dev.to see values.yaml for username & paasword
+# cat apps/wordpress/values.yaml
 . distros/archlinux/install.sh 2>&1 | tee logs/install_archlinux.log
+# watch kubectl -n archlinux get all
+# kubectl api-resources
+
