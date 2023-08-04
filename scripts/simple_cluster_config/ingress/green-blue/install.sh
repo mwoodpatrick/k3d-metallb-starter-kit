8111120f9038e661633a0fb2216288025f5f4217
mwoodpatrick <mwoodpatrick@gmail.com>
2023-08-03 21:18:23 -0700

    more fixes for green blue ingress test

--- scripts/simple_cluster_config/ingress/green-blue/install.sh
+++ scripts/simple_cluster_config/ingress/green-blue/install.sh
@@ -11,16 +11,25 @@ kubectl -n $GREEN_BLUE_INGRESS_NS apply -f $ingress_green_blue_dir/../../configm
 echo "waiting for green deployment"
 kubectl -n $GREEN_BLUE_INGRESS_NS wait --for=condition=Available deployment/green-web
 
+kubectl -n $GREEN_BLUE_INGRESS_NS expose deployment/green-web
+
+kubectl  -n $GREEN_BLUE_INGRESS_NS port-forward deployment/green-web :80 &
+
 # deplouy blue web app
 
 kubectl -n $GREEN_BLUE_INGRESS_NS create configmap blue-web-cm --from-file=$ingress_green_blue_dir/../../configmaps/blue/index.html
 
 kubectl -n $GREEN_BLUE_INGRESS_NS apply -f $ingress_green_blue_dir/../../configmaps/blue/web-blue-with-cm.yaml
 echo "waiting for blue deployment"
-kubectl -n $GREEN_BLUE_INGRESS_NS wait --for=condition=Available deployment/green-web
+kubectl -n $GREEN_BLUE_INGRESS_NS wait --for=condition=Available deployment/blue-web
+
+kubectl -n $GREEN_BLUE_INGRESS_NS expose deployment/blue-web
+
+kubectl  -n $GREEN_BLUE_INGRESS_NS port-forward deployment/blue-web :80 &
 
 # apply ingress
 
 kubectl -n $GREEN_BLUE_INGRESS_NS apply -f $ingress_green_blue_dir/virtual-host-ingress.yaml
 
+kubectl -n $GREEN_BLUE_INGRESS_NS describe ing/fan-out-ingress
 set +x
