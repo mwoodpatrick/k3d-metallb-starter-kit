2b041260b65068c2e47f3e84cfb55824b227e8da
mwoodpatrick <mwoodpatrick@gmail.com>
2023-08-07 00:54:05 -0700

    update mariadb

--- scripts/simple_cluster_config/apps/mariadb/install.sh
+++ scripts/simple_cluster_config/apps/mariadb/install.sh
@@ -1,9 +1,19 @@
 set -x
+
+MARIADB_INSTALL_DIR=$(realpath $( dirname "${BASH_SOURCE[0]}" ))
 export MARIADB_NS=mariadb
 helm install bitnami/mariadb --namespace $MARIADB_NS --create-namespace --generate-name
 MARIADB_POD_NAME=`kubectl -n mariadb get pod -l app.kubernetes.io/name=mariadb -o jsonpath='{.items[*].metadata.name}'`
+MARIADB_SVC_NAME=`kubectl -n mariadb get svc -l app.kubernetes.io/name=mariadb -o jsonpath='{.items[*].metadata.name}'`
 kubectl -n $MARIADB_NS wait --for=condition=Ready pod/$MARIADB_POD_NAME
-echo root password=$(kubectl get secret --namespace $MARIADB_NS $MARIADB_POD_NAME -o jsonpath="{.data.mariadb-root-password}" | base64 -d)
+
+MARIADB_ROOT_PASSWORD=$(kubectl get secret --namespace $MARIADB_NS $MARIADB_SVC_NAME -o jsonpath="{.data.mariadb-root-password}" | base64 -d)
 echo kubectl -n $MARIADB_NS exec -it $MARIADB_POD_NAME -- bash
+
+echo kubectl -n mariadb exec -it $MARIADB_POD_NAME -- mysql -u root -p
+
+echo MARIADB_ROOT_PASSWORD=$MARIADB_ROOT_PASSWORD
+
+echo MARIADB_ROOT_PASSWORD=$MARIADB_ROOT_PASSWORD > $MARIADB_INSTALL_DIR/root_password
 
 set +x
