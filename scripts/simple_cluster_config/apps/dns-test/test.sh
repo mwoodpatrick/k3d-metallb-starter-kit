c162c7e4840627218d06bd0f7e6cc2f7b94a02de
mwoodpatrick <mwoodpatrick@gmail.com>
2023-08-08 09:21:34 -0700

    fix issues with dns test, add k3d registry

--- scripts/simple_cluster_config/apps/dns-test/test.sh
+++ scripts/simple_cluster_config/apps/dns-test/test.sh
@@ -1,6 +1,14 @@
 # [Debugging DNS Resolution](https://kubernetes.io/docs/tasks/administer-cluster/dns-debugging-resolution/)
-kubectl apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml
-kubectl get pods dnsutils
-kubectl exec -i -t dnsutils -- nslookup kubernetes.default
-kubectl exec -i -t dnsutils -- nslookup geo.mirror.pkgbuild.com
-kubectl exec -i -t dnsutils -- nslookup google.com
+set -x
+clusterdir=$(realpath $( dirname "${BASH_SOURCE[0]}" ))
+export DNS_NS=dnstest
+kubectl create ns $DNS_NS
+# kubectl -n $DNS_NS apply -f https://k8s.io/examples/admin/dns/dnsutils.yaml --wait=true --namepsace $DNS_NS
+kubectl -n $DNS_NS apply -f $clusterdir/dnsutils.yaml 
+kubectl -n $DNS_NS wait --for=condition=Ready pod/dnsutils
+kubectl -n $DNS_NS get pods dnsutils
+kubectl -n $DNS_NS exec -i -t dnsutils -- nslookup kubernetes.default
+kubectl -n $DNS_NS exec -i -t dnsutils -- nslookup geo.mirror.pkgbuild.com
+kubectl -n $DNS_NS exec -i -t dnsutils -- nslookup google.com
+kubectl delete ns $DNS_NS
+set +x
--- scripts/simple_cluster_config/cluster/k3d/install.sh
+++ scripts/simple_cluster_config/cluster/k3d/install.sh
@@ -13,7 +13,7 @@ then
     export K3D_CLUSTER_NAME=westie-dev
     export CLUSTER_NAME=k3d-${K3D_CLUSTER_NAME}
     k3d --version
-    k3d cluster create $K3D_CLUSTER_NAME --config $clusterdir/cluster-config.yaml --wait
+    k3d cluster create $K3D_CLUSTER_NAME --registry-create westie-registry --config $clusterdir/cluster-config.yaml --wait
     # k3d cluster create westie-dev --servers 1 --agents 3 --k3s-arg "--disable=traefik@server:0" --wait --volume /run/udev:/run/udev --volume /mnt/wsl/projects:/projects
     kubectl -n kube-system wait deployment.apps/metrics-server --for=condition=Available
     time kubectl -n kube-system wait apiservices v1beta1.metrics.k8s.io  --for=condition=Available --timeout=5m
